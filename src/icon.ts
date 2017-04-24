9ef3069c7000aea73fb134c6df6ad5a7cd9ce350
poltavets.pavel <pavel.poltavets@devexpress.com>
2017-04-24 13:34:04 +0300

    New icon design

--- src/icon.ts
+++ src/icon.ts
@@ -1,15 +1,18 @@
 ﻿/// <reference path="meta.ts" />
 
 module CustomItems {
-    export var ONLINE_MAP_ICON = 
+    export var ONLINE_MAP_ICON =
         `<?xml version="1.0" encoding="utf-8"?>
-        <svg version="1.1" id="`+ onlineMapMeta.icon +`" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
-             x="0px" y="0px" viewBox="4 -4 24 24" style="enable-background:new 4 -4 24 24;" xml:space="preserve">
-            <style type="text/css">
-	            .dx_ruby{fill:#FA7864;}
-            </style>
-            <circle class="dx_ruby" cx="19" cy="13" r="3"/>
-            <circle class="dx_ruby" cx="18" cy="5" r="3"/>
-            <circle class="dx_ruby" cx="11" cy="3" r="3"/>
-        </svg>`;
+<!-- Generator: Adobe Illustrator 21.0.2, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg version="1.1" id="`+ onlineMapMeta.icon +`" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve">
+<style type="text/css">
+	.dx_red{fill:#E25454;}
+	.dx_darkgray{fill:#414141;}
+</style>
+<path class="dx_darkgray" d="M12,1C8.1,1,5,4.1,5,8c0,3.9,3,10,7,15c4-5,7-11.1,7-15C19,4.1,15.9,1,12,1z M12,12c-2.2,0-4-1.8-4-4
+	c0-2.2,1.8-4,4-4s4,1.8,4,4C16,10.2,14.2,12,12,12z"/>
+<circle class="dx_red" cx="12" cy="8" r="2"/>
+</svg>`;
 }

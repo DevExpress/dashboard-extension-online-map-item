eca53ecc23846010293388d0a3f9a1af2199fd36
Bobrova Alena <alena.bobrova@devexpress.com>
2018-05-23 17:01:14 +0300

    remove inline css styles

--- src/icon.ts
+++ src/icon.ts
@@ -7,10 +7,6 @@ module CustomItems {
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg version="1.1" id="`+ onlineMapMeta.icon +`" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve">
-<style type="text/css">
-	.dx_red{fill:#E25454;}
-	.dx_darkgray{fill:#414141;}
-</style>
 <path class="dx_darkgray" d="M12,1C8.1,1,5,4.1,5,8c0,3.9,3,10,7,15c4-5,7-11.1,7-15C19,4.1,15.9,1,12,1z M12,12c-2.2,0-4-1.8-4-4
 	c0-2.2,1.8-4,4-4s4,1.8,4,4C16,10.2,14.2,12,12,12z"/>
 <circle class="dx_red" cx="12" cy="8" r="2"/>
